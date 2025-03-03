ab116086164247bc25f3f03f014fe7a06954921e
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-03-03 10:06:15 +0100

    lite mer funktionell

--- src/Inlamning_2/AoC2020_1a.kt
+++ src/Inlamning_2/AoC2020_1a.kt
@@ -4,27 +4,26 @@ import java.io.File
 
 //AoC 2020 day 1(A)
 //Find the two entries that sum to 2020; what do you get if you multiply them together?
+fun main() {
 
-private fun findEntries() {
-    val data: List<Int> = File("src/Inlamning_2/dataAoC1.txt").readLines().map { row -> row.toInt() }
-    val sum = 2020
+    fun multiply(entry1: Int, entry2: Int) {
+        println("The numbers are $entry1 and $entry2.")
+        println("The product is " + entry1 * entry2 + ".")
+    }
 
-    for (i in 0 until data.size) {
-        for (j in i + 1 until data.size) {
-            val entry1 = data[i]
-            val entry2 = data[j]
-            if (entry1 + entry2 == sum) {
-                multiply(entry1, entry2)
+    fun findEntries() {
+        val data: List<Int> = File("src/Inlamning_2/dataAoC1.txt").readLines().map { row -> row.toInt() }
+        val sum = 2020
+        for (i in 0 until data.size) {
+            for (j in i + 1 until data.size) {
+                val entry1 = data[i]
+                val entry2 = data[j]
+                if (entry1 + entry2 == sum) {
+                    multiply(entry1, entry2)
+                }
             }
         }
     }
-}
-
-private fun multiply(entry1: Int, entry2: Int) {
-    println("The numbers are $entry1 and $entry2.")
-    println("The product is " + entry1 * entry2 + ".")
-}
-
-fun main() {
+    
     findEntries()
 }
--- src/Inlamning_2/AoC2020_1b.kt
+++ src/Inlamning_2/AoC2020_1b.kt
@@ -5,30 +5,31 @@ import java.io.File
 //AoC 2020 day 1(B)
 //Find the THREE entries that sum to 2020; what do you get if you multiply them together?
 
-private fun findEntries() {
-    val data: List<Int> = File("src/Inlamning_2/dataAoC1.txt").readLines().map { row -> row.toInt() }
-    val sum = 2020
+fun main() {
 
-    for (i in 0 until data.size) {
-        for (j in i + 1 until data.size) {
-            for (k in j + 1 until data.size) {
-                val entry1 = data[i]
-                val entry2 = data[j]
-                val entry3 = data[k]
+    fun multiply(entry1: Int, entry2: Int, entry3: Int) {
+        println("The numbers are $entry1 and $entry2 and $entry3.")
+        println("The product is " + entry1 * entry2 * entry3 + ".")
+    }
 
-                if (entry1 + entry2 + entry3 == sum) {
-                    multiply(entry1, entry2, entry3)
+    fun findEntries() {
+        val data: List<Int> = File("src/Inlamning_2/dataAoC1.txt").readLines().map { row -> row.toInt() }
+        val sum = 2020
+
+        for (i in 0 until data.size) {
+            for (j in i + 1 until data.size) {
+                for (k in j + 1 until data.size) {
+                    val entry1 = data[i]
+                    val entry2 = data[j]
+                    val entry3 = data[k]
+
+                    if (entry1 + entry2 + entry3 == sum) {
+                        multiply(entry1, entry2, entry3)
+                    }
                 }
             }
         }
     }
-}
 
-private fun multiply(entry1: Int, entry2: Int, entry3: Int) {
-    println("The numbers are $entry1 and $entry2 and $entry3.")
-    println("The product is " + entry1 * entry2 * entry3 + ".")
-}
-
-fun main() {
     findEntries()
 }
