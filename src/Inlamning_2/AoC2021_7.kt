0038ec4dc0d9ffef07be2b4f0fa3380029b98591
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-03-06 12:32:10 +0100

    småfix

--- src/Inlamning_2/AoC2021_7.kt
+++ src/Inlamning_2/AoC2021_7.kt
@@ -4,22 +4,22 @@ import java.io.File
 import kotlin.math.absoluteValue
 
 //AOC 2021 day 7(A)
-//How manu units fuel must be spent to move all crabs to the one horizontal position that require the least fuel to move to?
+//How many units fuel must be spent to move all crabs to the one horizontal position that require the least fuel to move to?
 
 fun main() {
     val data: String = File("src/Inlamning_2/dataAoC7.txt").readText()
 
-    //Lägger in input i en Map. Map där key är en viss nuvarande horisontell position, value är hur många krabbor är där.
+    //Key är en viss nuvarande horisontell position, value är hur många krabbor är där
     val crabs: Map<Int, Int> = data.split(",").map { a -> a.toInt() }.groupingBy { b -> b }.eachCount()
 
     fun lowestFuelCost(): Int {
         val minPosition = crabs.keys.minOrNull()!!
         val maxPosition = crabs.keys.maxOrNull()!!
 
-        //Tar en range, mappar genom att gå igenom alla entries och tar summan av total bränslemängd för varje position.
+        //Tar en range, mappar genom att gå igenom alla entries och tar summan av total bränslemängd för varje position
         val minFuelCost = (minPosition..maxPosition)
             .map { targetPosition -> crabs.entries.sumOf { (crabPosition, crabCount) -> (targetPosition - crabPosition).absoluteValue * crabCount }
-            }.min() //svaret
+            }.min()
 
         return minFuelCost
     }
